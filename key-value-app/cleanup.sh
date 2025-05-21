ad401678c8fdec2a22fa91d1dbaa59bc376f6250
Josh LeBlanc <joshcapitals07@gmail.com>
2025-05-21 16:57:16 +0200

    now we have a setup script, cleanup script, end .env files

---
+++ key-value-app/cleanup.sh
@@ -0,0 +1,25 @@
+source .env.db
+source .env.volume
+source .env.network
+
+
+if [ "$(docker ps -aq -f name=$DB_CONTAINER_NAME)" ]; then
+    echo "Removing container $DB_CONTAINER_NAME"
+    docker kill $DB_CONTAINER_NAMER
+else
+    echo "A container with the name $DB_CONTAINER_NAME does not exist. Skipping container deletion."
+fi
+
+if [ "$(docker volume ls -q -f name=$VOLUME_NAME)" ]; then
+    echo "Removing volume $VOLUME_NAME"
+    docker volume rm $VOLUME_NAME
+else
+    echo "A volume with the name $VOLUME_NAME does not exist. Skipping volume deletion."
+fi
+
+if [ "$(docker network ls -q -f name=$NETWORK_NAME)" ]; then
+    echo "Removing network $NETWORK_NAME"
+    docker network rm $NETWORK_NAME
+else
+    echo "A network with the name $NETWORK_NAME does not exist. Skipping network deletion."
+fi
--- key-value-app/start-db.sh
+++ key-value-app/start-db.sh
@@ -1,6 +1,6 @@
 MONGODB_IMAGE="mongodb/mongodb-community-server"
 MONGODB_TAG="7.0-ubuntu2204"
-CONTAINER_NAME="mongodb"
+source .env.db
 
 # root creds
 ROOT_USER="root-user"
@@ -12,15 +12,22 @@ KEY_VALUE_USER="key-value-user"
 KEY_VALUE_PASSWORD="key-value-password"
 
 # connectivity
+source .env.network
 LOCALHOST_PORT=27017
 CONTAINER_PORT=27017
-NETWORK_NAME="key-value-net"
 
 # Storage
-VOLUME_NAME="key-value-data"
 VOLUME_CONTAINER_PATH="/data/db"
+source .env.volume
 
-docker run -d --rm --name $CONTAINER_NAME \
+source setup.sh
+
+if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
+    echo "A container with the name $DB_CONTAINER_NAME already exists."
+    exit 1
+fi
+
+docker run -d --rm --name $DB_CONTAINER_NAME \
     -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
     -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
     -e KEY_VALUE_DB=$KEY_VALUE_DB \
